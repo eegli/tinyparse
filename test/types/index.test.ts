1d7245929732ee285c66cd240f845605b99ef2cb
Eric Egli <43848365+eegli@users.noreply.github.com>
2022-12-27 17:15:20 +0100

    integrate type tests in ci

--- test/types/index.test.ts
+++ test/types/index.test.ts
@@ -7,6 +7,14 @@ type Input = {
   loggedIn: boolean;
 };
 
+expectType<Promise<Input>>(
+  createParser<Input>({
+    name: 'eric',
+    age: 11,
+    loggedIn: false,
+  }).parse()
+);
+
 expectAssignable<Promise<Input>>(
   createParser({
     name: 'eric',
@@ -15,10 +23,28 @@ expectAssignable<Promise<Input>>(
   }).parse()
 );
 
-expectType<Promise<Input>>(
-  createParser<Input>({
-    name: 'eric',
-    age: 11,
-    loggedIn: false,
-  }).parse()
-);
+type Options = Parameters<typeof createParser<Input>>[1];
+type OptionKeys = NonNullable<Options>['options'];
+
+expectAssignable<Options>({});
+expectAssignable<Options>({
+  filePathArg: {
+    longFlag: '--file' as const,
+  },
+});
+expectAssignable<Options>({
+  options: {},
+});
+expectAssignable<OptionKeys>({
+  name: {
+    required: true,
+    description: 'The name of the user',
+    shortFlag: `-n` as const,
+    customValidator: {
+      isValid: () => true,
+      errorMessage: () => 'Error',
+    },
+  },
+  age: {},
+  loggedIn: {},
+});
